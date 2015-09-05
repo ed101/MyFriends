9d133f632495299c53ee478530395fac703eefa7
vladislav <kirinsnake@gmail.com>
2015-09-05 23:56:29 +0300

    Initial Commit

---
+++ MyFriends/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFriends
+//
+//  Created by vladislav on 05.09.15.
+//  Copyright (c) 2015 vladislav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
